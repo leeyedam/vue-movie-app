e7392603a1f254c3382ac6069acfa80b1d28b479
leeyedam <dldpeka0909@gmail.com>
2022-01-01 22:54:13 +0900

    Modify server port number

--- src/store/movie.js
+++ src/store/movie.js
@@ -59,7 +59,7 @@ export default{
             })
           }
         }
-      } catch(message){
+      } catch({message}){
         commit('updateState',{
           movies: [],
           message
@@ -94,25 +94,8 @@ export default{
   }  
 }
 
-function _fetchMovie(payload){
-  const {title,type, year, page, id} = payload
-  const OMDB_API_KEY = '7035c60c'
-  const url = id
-  ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
-  : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-  return new Promise((resolve, reject)=>{
-    axios.get(url)
-      .then((res)=>{
-        if(res.data.Error){
-          reject(res.data.Error)
-        }
-        resolve(res)
-      })
-      .catch((err)=>{
-        reject(err.message)
-      })
-  })
+async function _fetchMovie(payload){
+  return await axios.post('/.netlify/functions/movie', payload)
 }
 //context: state,getters,mutation을 활용할 수 있는 몇가지 내용이 담겨져 있다.
 //payload: 인수로 들어온 특정한 데이터들을 받아줌
